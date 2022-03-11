bb2957166a561df4243d4cef5ae819710ff38e8c
arifsptra <arif.sptrra@gmail.com>
2022-03-11 09:20:13 +0700

    practice using the echo command output

---
+++ Minggu01/perintahOutput.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <body>
+    <!-- Perintah Output echo -->
+    <?php
+        $example= "<h3>Arif Saputra</h3>";
+        echo "<p>Perkenalkan,</p>";
+        echo ("Nama Saya<br>");
+        echo $example;
+        echo "saya ", "orang ", "pati";
+    ?> 
+    </body>
+</html>
