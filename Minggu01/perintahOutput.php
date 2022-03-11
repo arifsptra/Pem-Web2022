e6e21982ddeaa0f2899a0d8763477439363397f5
arifsptra <arif.sptrra@gmail.com>
2022-03-11 09:34:04 +0700

    practice using the output print command

--- Minggu01/perintahOutput.php
+++ Minggu01/perintahOutput.php
@@ -8,6 +8,21 @@
         echo ("Nama Saya<br>");
         echo $example;
         echo "saya ", "orang ", "pati";
-    ?> 
+    ?>
+
+    <!-- Perintah Output print -->
+    <?php
+        $txt1 = "Universitas Dian Nuswantoro Semarang";
+        $txt2 = "S1 Teknik Informatika ";
+        $txt3 = "Fakultas Ilmu Komputer";
+        $x = "11";
+        $y = "93";
+        $ret = print("<br><br>Saya sekarang kuliah di ");
+        print "$txt1";
+        print "<br>Saya mengambil prodi $txt2 $txt3";
+        print "<br><br>Nomor kesukaan saya $x dan $y jika ditambahkan menjadi ";
+        print $x + $y;
+        print "<br>Print memiliki return sebesar: $ret";
+    ?>
     </body>
 </html>
