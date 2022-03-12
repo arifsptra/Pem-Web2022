7d7842da6c8af250c87f62d5e7c7c2ae74517940
arifsptra <arif.sptrra@gmail.com>
2022-03-12 10:31:48 +0700

    learn array, object, null, and resource data types

--- Minggu01/tipeData.php
+++ Minggu01/tipeData.php
@@ -38,5 +38,53 @@
     var_dump($benar);
     var_dump($salah);
     ?>
+
+    <!-- Array Data Type -->
+    <?php
+    $nama = array("Arif", "Uswa", "Askary");
+    echo "<p>Kita adalah $nama[0], $nama[1], dan $nama[2]</p>";
+    var_dump($nama);
+    ?>
+
+    <!-- Object Data Type -->
+    <?php
+    class Family {
+        public $namaLengkap;
+        public $asal;
+        public $hobi;
+        public function __construct($namaLengkap, $asal, $hobi) {
+            $this -> namaLengkap = $namaLengkap;
+            $this -> asal = $asal;
+            $this -> hobi = $hobi;
+        }
+        public function message(){
+            return "<p>Nama saya " .$this -> namaLengkap. " asal saya dari " .$this -> asal. " saya memiliki hobi " .$this -> hobi. "</p>";
+        }
+    }
+    $member = new Family("Arif Saputra", "Pati", "Bermain Sepak Bola");
+    echo $member->message();
+    $member = new Family("Fajriah Uswatun Hasanah", "Pati", "Memasak");
+    echo $member->message();
+    var_dump($member);
+    ?>
+
+    <!-- NULL Data Type -->
+    <?php
+    $kosong = NULL;
+
+    echo "<p>Data nya $kosong</p>";
+    var_dump($kosong);
+    ?>    
+
+    <!-- Resource Data Type -->
+    <?php
+    // $connect = mysqli_connect($servername, $username, $password, $db_name);
+    $connect = mysqli_connect("localhost", "root", "", "websiteku");
+    $file = fopen('forFopen.php', 'r') or die('File tidak dapat dibuka!');
+    echo fgets($file);
+    // echo fread($file, filesize('forFopen.php'));
+    // fclose($file);
+    // fread($file, filesize('forFopen.php'));
+    ?>
     </body>
 </html>
