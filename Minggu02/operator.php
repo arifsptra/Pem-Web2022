b318d502da03c9068bb03ea33d6c53a382af3e43
arifsptra <arif.sptrra@gmail.com>
2022-03-18 10:52:06 +0700

    practicum using assignment operator

--- Minggu02/operator.php
+++ Minggu02/operator.php
@@ -18,6 +18,26 @@
         echo "Hasil $a % $b : ", $a%$b, "<br>";
         // Exponential
         echo "Hasil $a ** $b : ", $a**$b, "<br>";
+
+        // Assignment Operators
+        echo "<p><strong>Aritmatic</strong> Operators: <p>";
+        $a = 11;
+        $b = 2;
+        echo "Diketahui Nilai Awal A = 11 dan B = 2 <br>";
+        // Assign values
+        echo "Hasil A=B : ", $a=$b, "<br>";
+        // Addition
+        echo "Hasil A+=B : ", $a+=$b, "<br>";
+        // Subtraction
+        echo "Hasil A-=B : ", $a-=$b, "<br>";
+        // Multiplication
+        echo "Hasil A*=B : ", $a*=$b, "<br>";
+        // Division
+        echo "Hasil A/=B : ", $a/=$b, "<br>";
+        // Modulus
+        echo "Hasil A%=B : ", $a%=$b, "<br>";
+        // Exponential
+        echo "Hasil A**=B : ", $a**=$b, "<br>";
     ?>
     </body>
 </html>
