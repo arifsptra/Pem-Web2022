49c102a4bb433bd33abb57329528d0b251d2504f
arifsptra <arif.sptrra@gmail.com>
2022-03-18 11:07:50 +0700

    learn to use the comparison operator

--- Minggu02/operator.php
+++ Minggu02/operator.php
@@ -20,7 +20,7 @@
         echo "Hasil $a ** $b : ", $a**$b, "<br>";
 
         // Assignment Operators
-        echo "<p><strong>Aritmatic</strong> Operators: <p>";
+        echo "<p><strong>Assignment</strong> Operators: <p>";
         $a = 11;
         $b = 2;
         echo "Diketahui Nilai Awal A = 11 dan B = 2 <br>";
@@ -38,6 +38,31 @@
         echo "Hasil A%=B : ", $a%=$b, "<br>";
         // Exponential
         echo "Hasil A**=B : ", $a**=$b, "<br>";
+
+        // Comparison Operator
+        echo "<p><strong>Comparison</strong> Operators: <p>";
+        $a = 11;
+        $b = "93";
+        echo "the return is true(1) or false( )<br>";
+        echo "Diketahui Nilai Awal A = 11 (int) dan B = 93 (string) <br>";
+        // Equal
+        echo "Hasil A == B : ", $a==$b, "<br>";
+        // Identical
+        echo "Hasil A === B : ", $a===$b, "<br>";
+        // Not Equal
+        echo "Hasil A != B : ", $a!=$b, "<br>";
+        // Not Identical
+        echo "Hasil A !== B : ", $a!==$b, "<br>";
+        // Greater Than
+        echo "Hasil A > B : ", $a>$b, "<br>";
+        // Less Than
+        echo "Hasil A < B : ", $a<$b, "<br>";
+        // Greater Than or Equal to
+        echo "Hasil A >= B : ", $a>=$b, "<br>";
+        // Less Than or Equal to
+        echo "Hasil A <= B : ", $a<=$b, "<br>";
+        // Spaceship
+        echo "Hasil A <=> B : ", $a<=>$b, "<br>";
     ?>
     </body>
 </html>
