bc25898f6d5f52f2328b95e6bcc0395a4edb858b
arifsptra <arif.sptrra@gmail.com>
2022-03-18 10:39:25 +0700

    practice using arithmetic operators

---
+++ Minggu02/operator.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <body>
+    <?php
+        // Arithmetic Operators
+        echo "<p><strong>Aritmatic</strong> Operators: <p>";
+        $a = 10;
+        $b = 3;
+        // Addition
+        echo "Hasil $a + $b : ", $a+$b, "<br>";
+        // Subtraction
+        echo "Hasil $a - $b : ", $a-$b, "<br>";
+        // Multiplication
+        echo "Hasil $a * $b : ", $a*$b, "<br>";
+        // Division
+        echo "Hasil $a / $b : ", $a/$b, "<br>";
+        // Modulus
+        echo "Hasil $a % $b : ", $a%$b, "<br>";
+        // Exponential
+        echo "Hasil $a ** $b : ", $a**$b, "<br>";
+    ?>
+    </body>
+</html>
