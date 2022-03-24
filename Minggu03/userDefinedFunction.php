0d66a090e7a0e94e8e7e428ed52786d3258d39c2
arifsptra <arif.sptrra@gmail.com>
2022-03-24 20:11:41 +0700

    practice using function

---
+++ Minggu03/userDefinedFunction.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <body>
+    <?php
+    // User Defined Function
+    function sapa(){
+        echo "<p>Hello World!</p>";
+    }
+    sapa(); // calling the sapa function
+    ?>
+    </body>
+</html>
