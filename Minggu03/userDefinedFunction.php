e40366c0e2a973100554d4af5414bd8512d80c4a
arifsptra <arif.sptrra@gmail.com>
2022-03-24 20:26:39 +0700

    learn to use user-defined pass-by-value and pass-by-reference functions

--- Minggu03/userDefinedFunction.php
+++ Minggu03/userDefinedFunction.php
@@ -7,6 +7,19 @@
         echo "<p>Hello World!</p>";
     }
     sapa(); // calling the sapa function
+    
+    // pass-by-value
+    function nilai($n1){
+        echo $n1+=10;
+    }
+    nilai(10);
+
+    // pass-by-reference
+    function nilai2(&$n2){
+        echo $n2+=11;
+    }
+    $n3 = 11;
+    nilai2($n3); 
     ?>
     </body>
 </html>
