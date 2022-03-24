471e9f2b323d0f4c642ce680fa3493116421162a
arifsptra <arif.sptrra@gmail.com>
2022-03-24 20:39:29 +0700

    practicum using built-in-function string

---
+++ Minggu03/buildInFunction.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <body>
+    <?php
+    // string
+    // strlen() => return the lenght of a string
+    echo strlen("Arif Saputra"), "<br/>";
+    // str_word_count() => count word in string
+    echo str_word_count("Arif Saputra"), "<br/>";
+    // strrev() => reverse a string
+    echo strrev("Arif Saputra"), "<br/>";
+    // strpos() => search for a text within a string
+    echo strpos("Arif Saputra", "Saputra"), "<br/>";
+    // str_replace() => replace text within a string
+    echo str_replace("Arif", "Boy", "Hello the little Arif!");
+    ?>
+    </body>
+</html>
