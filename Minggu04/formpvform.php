96f77100861a1da3baf27109198ebbf5143606c4
arifsptra <arif.sptrra@gmail.com>
2022-04-01 20:42:30 +0700

    practicum using message passing via form

---
+++ Minggu04/formpvform.php
@@ -0,0 +1,5 @@
+<?php
+    $user = $_POST['user'];
+    $pass = $_POST['pass'];
+    echo "User: " . $user;
+?>
---
+++ Minggu04/mpvform.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Message Passing via Form</title>
+</head>
+<body>
+    <form action="formpvform.php" method="POST">
+        <label for="user">User</label><br>
+        <input type="text" name="user"><br>
+        <label for="pass">Password</label><br>
+        <input type="password" name="pass"><br>
+        <input type="submit" value="login">
+    </form>
+</body>
+</html>
