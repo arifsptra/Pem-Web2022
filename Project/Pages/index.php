72a4b936cc37e2561dcb9ff140f3f1c6f85b1b8d
arifsptra <arif.sptrra@gmail.com>
2022-04-12 22:15:35 +0700

    make navbar

--- Project/Pages/index.php
+++ Project/Pages/index.php
@@ -19,5 +19,32 @@
     <title>Kutani | Platform Pembantu Petani</title>
   </head>
   <body>
+    <!-- Start Navbar -->
+    <header id="navbar">
+        <div class="container">
+            <div class="navLogo">
+                <img src="../Images/logoKutani.svg" alt="kutani">
+            </div>
+            <div class="navList">
+                <ul>
+                    <li><a href="../Pages/index.php">Home</a></li>
+                    <li><a href="../Pages/product.php">Product</a></li>
+                    <li><a href="../Pages/community.php">Community</a></li>
+                    <li><a href="../Pages/article.php">Article</a></li>
+                    <li><a href="../Pages/about.php">About</a></li>
+                </ul>
+            </div>
+            <div class="navIcon">
+                <div class="search"></div>
+                <div class="cart"></div>
+            </div>
+            <div class="line"></div>
+            <div class="NavRegister">
+                <div class="signIn"></div>
+                <div class="signOut"></div>
+            </div>
+        </div>
+    </header>
+    <!-- End Navbar -->
   </body>
 </html>
