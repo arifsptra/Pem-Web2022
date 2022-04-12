c6f955d6c4420b192bd18dc4a6127905889fc9a2
arifsptra <arif.sptrra@gmail.com>
2022-04-12 20:45:23 +0700

    basic configuration

---
+++ Project/Pages/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta author="Arif Saputra">
+
+    <!-- Google Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,500;0,600;1,500;1,600&display=swap" rel="stylesheet">
+
+    <!-- My Style CSS -->
+    <link rel="stylesheet" href="../Style/main.css">
+
+    <!-- Title -->
+    <link rel="icon" href="../Images/iconKutani.png">
+    <title>Kutani | Platform Pembantu Petani</title>
+  </head>
+  <body>
+  </body>
+</html>
