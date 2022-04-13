2514873a45b18c22670b5b7a7a772bd20103ceed
arifsptra <arif.sptrra@gmail.com>
2022-04-13 21:36:13 +0700

    fix navbar display for desktop

--- Project/Pages/index.php
+++ Project/Pages/index.php
@@ -1,49 +1,60 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta author="Arif Saputra">
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta author="Arif Saputra" />
 
     <!-- Google Font -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,500;0,600;1,500;1,600&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,500;0,600;1,500;1,600&display=swap" rel="stylesheet" />
+
+    <!-- Font Awesome -->
+    <script src="https://kit.fontawesome.com/09bdcabb66.js" crossorigin="anonymous"></script>
 
     <!-- My Style CSS -->
-    <link rel="stylesheet" href="../Style/main.css">
+    <link rel="stylesheet" href="../Style/main.css" />
 
     <!-- Title -->
-    <link rel="icon" href="../Images/iconKutani.png">
+    <link rel="icon" href="../Images/iconKutani.png" />
     <title>Kutani | Platform Pembantu Petani</title>
   </head>
   <body>
     <!-- Start Navbar -->
     <header id="navbar">
-        <div class="container">
-            <div class="navLogo">
-                <img src="../Images/logoKutani.svg" alt="kutani">
-            </div>
-            <div class="navList">
-                <ul>
-                    <li><a href="../Pages/index.php">Home</a></li>
-                    <li><a href="../Pages/product.php">Product</a></li>
-                    <li><a href="../Pages/community.php">Community</a></li>
-                    <li><a href="../Pages/article.php">Article</a></li>
-                    <li><a href="../Pages/about.php">About</a></li>
-                </ul>
-            </div>
-            <div class="navIcon">
-                <div class="search"></div>
-                <div class="cart"></div>
-            </div>
-            <div class="line"></div>
-            <div class="NavRegister">
-                <div class="signIn"></div>
-                <div class="signOut"></div>
-            </div>
+      <div class="container">
+        <div class="navLeft">
+          <div class="navLogo">
+            <a href="../Pages/index.php"><img src="../Images/logoKutani.svg" alt="kutani" /></a>
+          </div>
         </div>
+        <div class="navRight">
+          <div class="navList">
+            <ul>
+              <li><a href="../Pages/index.php">Home</a></li>
+              <li><a href="../Pages/product.php">Product</a></li>
+              <li><a href="../Pages/community.php">Community</a></li>
+              <li><a href="../Pages/article.php">Article</a></li>
+              <li><a href="../Pages/about.php">About</a></li>
+            </ul>
+          </div>
+          <div class="navIcon">
+            <a href=""><i class="fa-solid fa-magnifying-glass"></i></a>
+            <a href=""><i class="fa-solid fa-cart-shopping"></i></a>
+          </div>
+          <div class="navLine"></div>
+          <div class="navButton">
+            <button class="signIn">
+              <a href="../Pages/Reg/in.php">Sign In</a>
+            </button>
+            <button class="signUp">
+              <a href="../Pages/Reg/up.php">Sign Up</a>
+            </button>
+          </div>
+        </div>
+      </div>
     </header>
     <!-- End Navbar -->
   </body>
