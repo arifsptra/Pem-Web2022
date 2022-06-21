0f81f2d1d100ebd96ce4b22e5407478a6a4d621d
arifsptra <arif.sptrra@gmail.com>
2022-06-21 15:14:40 +0700

    access modified practicum

--- Minggu08/classBuah.php
+++ Minggu08/classBuah.php
@@ -21,5 +21,10 @@ class Buah {
     function __destruct(){
         echo "Buah {$this->nama} telah dihancurkan";
     }
+
+    // Access Modified
+    public $var1; // accessible to anyone
+    private $var2; // can only be accessed in class/local only
+    protected $var3; // can be accessed within a class and by classes that derive from that class
 }
 ?>
