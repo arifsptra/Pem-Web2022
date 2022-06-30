832994e0ff6a2fc956e9f8d4431aeca7170598aa
arifsptra <arif.sptrra@gmail.com>
2022-06-30 14:23:55 +0700

    make display bantuan.php

---
+++ Project/bantuan.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <!-- Google Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,500;0,600;1,500;1,600&display=swap" rel="stylesheet" />
+
+    <!-- Font Awesome -->
+    <script src="https://kit.fontawesome.com/09bdcabb66.js" crossorigin="anonymous"></script>
+
+    <!-- My Style CSS -->
+    <link rel="stylesheet" href="./css/main.css" />
+
+    <!-- Title -->
+    <link rel="icon" href="./img/iLogo.svg" />
+    <title>Kutani | Bantuan</title>
+  </head>
+  <body>
+    <div id="bantuan">
+      <div class="container">
+        <h1>Butuh <strong>Bantuan?</strong></h1>
+        <div class="hub">
+          <p>Hubungi <a href="https://wa.me/6281324475366">081324475366</a></p>
+          <p class="name">Arif Saputra</p>
+        </div>
+        <p>Kembali ke <a href="index.php">Beranda</a></p>
+      </div>
+    </div>
+  </body>
+</html>
